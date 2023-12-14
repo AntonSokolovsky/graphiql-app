e499a387596587a7093fae1fd9eb316062d546aa
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2023-12-14 11:49:10 +0300

    fix: move the secret inforamtion to .env

--- src/services/auth/firebase.ts
+++ src/services/auth/firebase.ts
@@ -1,61 +1,37 @@
 import { initializeApp } from 'firebase/app';
 import {
-  GoogleAuthProvider,
   getAuth,
-  signInWithPopup,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   sendPasswordResetEmail,
   signOut,
 } from 'firebase/auth';
-import {
-  getFirestore,
-  query,
-  getDocs,
-  collection,
-  where,
-  addDoc,
-} from 'firebase/firestore';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 const dbPaths = {
   users: 'users',
   uid: 'uid',
 };
 
+const apiKey = process.env.API_KEY || '';
+const authDomain = process.env.AUTH_DOMAIN || '';
+const projectId = process.env.PROJECT_ID || '';
+const storageBucket = process.env.STORAGE_BUCKET || '';
+const messagingSenderId = process.env.MESSAGING_SENDER_ID || '';
+const appId = process.env.APP_ID || '';
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyAltm1V5tqCz-UUtgDrnEXMHfmcy4zqm4c',
-  authDomain: 'graphiql-app-a040a.firebaseapp.com',
-  projectId: 'graphiql-app-a040a',
-  storageBucket: 'graphiql-app-a040a.appspot.com',
-  messagingSenderId: '226221838512',
-  appId: '1:226221838512:web:c62782bc957bab2793ab75',
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-const googleProvider = new GoogleAuthProvider();
-export const signInWithGoogle = async () => {
-  try {
-    const res = await signInWithPopup(auth, googleProvider);
-    const user = res.user;
-    const q = query(
-      collection(db, dbPaths.users),
-      where(dbPaths.uid, '==', user.uid)
-    );
-    const docs = await getDocs(q);
-    if (!docs.docs.length) {
-      await addDoc(collection(db, dbPaths.users), {
-        uid: user.uid,
-        name: user.displayName,
-        authProvider: 'google',
-        email: user.email,
-      });
-    }
-  } catch (err) {
-    if (err instanceof Error) throw new Error(err.message);
-  }
-};
 
 export const logInWithEmailAndPassword = async (
   email: string,
